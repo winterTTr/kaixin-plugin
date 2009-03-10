98e449b9a3c6ac7f27477f25ba228372606c1459
winterTTr <winterTTr@661a9c9c-0d31-11de-b55b-d397e7115e68>
2009-03-10 08:36:42 +0000

    Add FriendList Ctrl , Update Check Action into inner data

--- kxData.py
+++ kxData.py
@@ -4,19 +4,16 @@ __version__ = '$Revision$'[11:-2]
 __author__ = 'winterTTr<winterTTr@gmail.com>'
 __svnid__ = '$Id$'
 
-import os
-import sys
-import urllib
-import urllib2
+import os , sys , re
+import urllib , urllib2 , cookielib
 from xml.etree import ElementTree as ET
-import re
 
 global_unicode_convert = re.compile( '\\\\u(?P<uchar>[\da-f]{4})')
 global_friendslist_x = re.compile( r'"uid":(?P<uid>\d+),"real_name":"(?P<real_name>[\\\da-fu]+)","real_name_unsafe":"(?P<real_name_unsafe>[\\\da-fu]+)"' )
 
 def SendRequest( request_name , **kwdict ):
-    global global_config_info
-    request_info = global_config_info.getRequestInfo( request_name , **kwdict )
+    global global_local_config_info
+    request_info = global_local_config_info.getRequestInfo( request_name , **kwdict )
     assert request_info , 'No info for request[%s]' % request_name
 
     if request_info['params'] :
@@ -44,13 +41,70 @@ def UnicodeFromStr( s ):
     global global_unicode_convert
     return global_unicode_convert.sub( ( lambda mo : unichr( int(mo.group('uchar') ,16 ) ) ), s )
 
-class ConfigureInfoLocal:
+class kxFriendsList:
+    __file_name__ = 'friendslist.xml'
+    __xml_index__ = [ 'id' , 'real_name' , 'real_name_unsafe' , 'steal']
+    def __init__( self ):
+        self.flist = []
+        if os.path.exists( self.__file_name__ ):
+            self.LoadFromFile()
+        else:
+            self._getListByRequset()
+            self.UpdateToFile()
+
+    def _getListByRequset( self ):
+        self.flist = []
+        resp = SendRequest( 'FriendsList' )
+        for x in global_friendslist_x.finditer( resp.read() ):
+            self.flist.append( 
+                    [ 
+                        int(x.group('uid') ),  
+                        UnicodeFromStr( x.group('real_name') ) , 
+                        UnicodeFromStr( x.group('real_name_unsafe') ) , 
+                        0  
+                    ])
+
+    def UpdateToFile( self ):
+        root = ET.Element('FriendsList')
+        for x in self.flist:
+            each = ET.SubElement( root , 'person' )
+            ET.SubElement( each , self.__xml_index__[0] ).text = str( x[0] )
+            ET.SubElement( each , self.__xml_index__[1] ).text = x[1]
+            ET.SubElement( each , self.__xml_index__[2] ).text = x[2]
+            ET.SubElement( each , self.__xml_index__[3] ).text = str ( x[3] )
+
+        et = ET.ElementTree( element = root)
+        et.write( self.__file_name__ , encoding='utf-8')
+
+
+    def LoadFromFile( self ):
+        self.flist = []
+        et = ET.ElementTree( file = self.__file_name__ )
+        for x in et.findall( 'person' ):
+            self.flist.append(
+                    [
+                        int( x.find( self.__xml_index__[0] ).text ),
+                        x.find( self.__xml_index__[1] ).text ,
+                        x.find( self.__xml_index__[2] ).text ,
+                        int(  x.find( self.__xml_index__[3] ).text ) 
+                    ] )
+
+    def GetList(self):
+        return self.flist
+
+    def SetSteal( self , id , steal):
+        for x in self.flist:
+            if x[0] == id:
+                x[3] = 1 if steal else 0
+                break
+
+class RequestInfoSet:
     def __init__( self ):
         self.ET = None
         self.request_list = None
         self.attrib_list = None
 
-    def loadFromFile( self , filename ):
+    def loadFromFile( self , filename = 'config.xml' ):
         assert os.path.exists( filename ) , 'there is no config file'
         self.ET = ET.ElementTree( file = filename )
         self.request_list = self.ET.find('request_list')
@@ -101,7 +155,7 @@ class ConfigureInfoLocal:
             result_list.append( x.text )
         return result_list
 
-class ConfigureInfoNetwork :
+class DynamicInfoSet :
     def __init__ ( self ):
         self.verify = ''
 
@@ -116,65 +170,23 @@ class ConfigureInfoNetwork :
         SendRequest( 'Login' , email = email , password = passwd)
 
         # get verify
-        resp = kxData.SendRequest( 'Verify' )
+        resp = SendRequest( 'Verify' )
         content = resp.read()
         self.verify = re.search( 'var g_verify = "(?P<verify>.*)";' , content ).group('verify')
 
+        # getFriends List
+        self.friends_list = kxFriendsList()
+
+
     def logout( self ):
-        kxData.SendRequest( 'Logout' )
+        SendRequest( 'Logout' )
 
 
-global_local_config_info = ConfigureInfoLocal()
-global_network_config_info = ConfigureInfoNetwork()
-
-
-
-class kxFriendsList:
-    __file_name__ = 'friendslist.xml'
-    __xml_index__ = [ 'id' , 'real_name' , 'real_name_unsafe' ]
-    def __init__( self ):
-        self.flist = []
-        if os.path.exists( self.__file_name__ ):
-            self._loadFromFile()
-        else:
-            self._getListByRequset()
-
-    def _getListByRequset( self ):
-        self.flist = []
-        resp = SendRequest( 'FriendsList' )
-        for x in global_friendslist_x.finditer( resp.read() ):
-            self.flist.append( 
-                    [ 
-                        int(x.group('uid') ),  
-                        UnicodeFromStr( x.group('real_name') ) , 
-                        UnicodeFromStr( x.group('real_name_unsafe') )
-                    ])
-        root = ET.Element('FriendsList')
-        for x in self.flist:
-            each = ET.SubElement( root , 'person' )
-            ET.SubElement( each , self.__xml_index__[0] ).text = str( x[0] )
-            ET.SubElement( each , self.__xml_index__[1] ).text = x[1]
-            ET.SubElement( each , self.__xml_index__[2] ).text = x[2]
-
-        et = ET.ElementTree( element = root)
-        et.write( self.__file_name__ , encoding='utf-8')
+global_local_config_info = RequestInfoSet()
+global_network_config_info = DynamicInfoSet()
 
 
 
-    def _loadFromFile( self ):
-        self.flist = []
-        et = ET.ElementTree( file = self.__file_name__ , )
-        for x in et.findall( 'person' ):
-            self.flist.append(
-                    [
-                        int( x.find( __xml_index__[0] ).text ),
-                        x.find( __xml_index__[0] ).text ,
-                        x.find( __xml_index__[1] ).text ,
-                        x.find( __xml_index__[2] ).text 
-                    ] )
-
-    def getList(self):
-        return self.flist
 
 
 
--- main.py
+++ main.py
@@ -5,23 +5,63 @@ __author__ = 'winterTTr<winterTTr@gmail.com>'
 __svnid__ = '$Id$'
 
 
-import wx
-import sys 
+import wx , sys , re
+import kxData
 from StringIO import StringIO
 from xml.etree import ElementTree as ET
-import kxData
-import re
-
 from wx.lib.mixins.listctrl import CheckListCtrlMixin , ListCtrlAutoWidthMixin
+
+class CheckListCtrl(wx.ListCtrl, CheckListCtrlMixin, ListCtrlAutoWidthMixin):
+    def __init__(self, parent):
+        wx.ListCtrl.__init__(self, parent, -1, style= wx.LC_SINGLE_SEL | wx.LC_REPORT | wx.SUNKEN_BORDER)
+        CheckListCtrlMixin.__init__(self)
+        ListCtrlAutoWidthMixin.__init__(self)
+        self.list_manager = None
+
+
+    def SetListManager( self , lm ):
+        self.list_manager = lm
+
+    def OnCheckItem( self , index , check ):
+        id = int ( self.GetItem( index , 1 ).GetText() )
+        self.list_manager.SetSteal( id , check )
+
+    def UpdateList( self ):
+        self.DeleteAllItems()
+        for index , content in enumerate ( self.list_manager.GetList() ): 
+            self.InsertStringItem( sys.maxint , '' )
+            self.SetStringItem( index , 0 ,  content[1] )
+            self.SetStringItem( index , 1 ,  unicode ( content[0] ) )
+            if content[3] == 1:
+                self.CheckItem( index , True )
+        
+
+
+
 
 class PageFriendsList(wx.Panel):
     def __init__(self, parent ):
         wx.Panel.__init__(self, parent)
+        sizer = wx.BoxSizer()
+        self.SetSizer ( sizer )
+        
+        # create list
+        self.list_ctrl = CheckListCtrl( self )
+        sizer.Add( self.list_ctrl , 1 , wx.EXPAND | wx.ALL , 10 )
 
+        index = 0
+        for x in [  [u"主人" , 200 ] , [u"主人ID" , 200] ]: 
+            self.list_ctrl.InsertColumn( index , x[0] , wx.LIST_FORMAT_LEFT , x[1] )
+            index += 1
 
+    def UpdateList( self ):
+        self.list_ctrl.UpdateList()
+
+    def SetDataManager( self , lm ):
+        self.list_ctrl.SetListManager( lm )
 
 class CropsListFrame( wx.Frame ):
-    def __init__( self , config_file_name = 'config.xml' ):
+    def __init__( self  ):
         wx.Frame.__init__( 
                 self , 
                 parent = None , 
@@ -29,105 +69,95 @@ class CropsListFrame( wx.Frame ):
                 title = u'开心菜园外挂 by winterTTr' ,
                 size = ( 800 , 600 ) )
 
+        ### ========== Init UI
+        # create item
         self.panel = wx.Panel( self )
         self.notebook = wx.Notebook( self.panel )
+        self.page_friendslist = PageFriendsList( self.notebook )
 
-        self.notebook.AddPage( PageFriendsList( self.notebook ) , u"好友设置")
-        self.notebook.AddPage( PageFriendsList( self.notebook ) , u"自动执行")
+        # setup item
+        self.notebook.AddPage( self.page_friendslist , u"好友设置")
+        #self.notebook.AddPage( PageFriendsList( self.notebook ) , u"自动执行")
 
+        # arrange item
         self.sizer = wx.BoxSizer()
         self.sizer.Add( self.notebook , 1 , wx.EXPAND )
         self.panel.SetSizer( self.sizer )
 
+
+        ### ========== Init Inner Data
+        # Get name and passwd
+        email = wx.GetTextFromUser( 
+                'Email:' , 
+                'Acount Information[Email]' , 
+                'winterTTr@hotmail.com' , 
+                self)
+        passwd = wx.GetPasswordFromUser(
+                'Password:',
+                'Acount Information[Password]',
+                'kaixin1983',
+                self)
+        if email == '' or passwd == '':
+            assert 0 , 'Invalid email or password'
         # init configure
-        #kxData.global_config_info.loadFromFile( config_file_name )
+        kxData.global_local_config_info.loadFromFile()
+        kxData.global_network_config_info.login( 
+                email = email ,
+                passwd = passwd)
         
-        ## create list
-        #self.list_ctrl = wx.ListCtrl( 
-        #        self , 
-        #        -1 , 
-        #        style = wx.LC_SINGLE_SEL | wx.LC_REPORT | wx.LC_HRULES | wx.LC_VRULES )
+        ### ========== Bind Event
+        self.Bind(wx.EVT_CLOSE, self.on_close)
 
-        ## int header
-        #index = 0 
-        ##for x in kxData.global_config_info.getAttribList():
-        ##    self.list_ctrl.InsertColumn( index , x , wx.LIST_FORMAT_LEFT , width )
-        ##    index += 1
-        #for x in [ [u"主人ID" , 100] , [u"主人" , 100] , [ u"成熟蔬菜id" , 100 ] , [ u"蔬菜状态" , 400] ]: 
-        #    self.list_ctrl.InsertColumn( index , x[0] , wx.LIST_FORMAT_LEFT , x[1] )
-        #    index += 1
-
-        ## Bind Event
-        #self.Bind(wx.EVT_CLOSE, self.on_close)
-
-        ## Get email and passwd
-        #self.email = wx.GetTextFromUser( 
-        #        'Email:' , 
-        #        'Acount Information[Email]' , 
-        #        'winterTTr@hotmail.com' , 
-        #        self)
-        #self.passwd = wx.GetPasswordFromUser(
-        #        'Password:',
-        #        'Acount Information[Password]',
-        #        'kaixin1983',
-        #        self)
-
-        #if self.email == '' or self.passwd == '':
-        #    assert 0 , 'Invalid email or password'
-
-        ## Login
-        #self.Login( self.email , self.passwd )
-        #self.GetVerify()
-        ## getFriendlist
-        #self.flist = kxData.kxFriendsList()
-
-
-        ##self.UpdateListInfo()
+        ### ========== Update UI
+        self.page_friendslist.SetDataManager( kxData.global_network_config_info.friends_list )
+        self.UpdateListInfo()
 
 
     def UpdateListInfo( self ):
-        for f_index in [ [ 0 , u"自己" , u"自己"] ] + self.flist.getList() :
-        #for f_index in [ [ 4287324 , u"自己" , u"自己"] ] :
-            resp = kxData.SendRequest( 'CropInfo' , fuid = f_index[0] , verify = self.verify )
-            sio = StringIO( resp.read() )
-            sio.seek(0)
-            if sio.getvalue()[0] != '<' :
-                continue
-            analyzor = ET.ElementTree( file = sio )
-            sio.close()
+        self.page_friendslist.UpdateList()
+        #for f_index in [ [ 0 , u"自己" , u"自己"] ] + self.flist.getList() :
+        ##for f_index in [ [ 4287324 , u"自己" , u"自己"] ] :
+        #    resp = kxData.SendRequest( 'CropInfo' , fuid = f_index[0] , verify = self.verify )
+        #    sio = StringIO( resp.read() )
+        #    sio.seek(0)
+        #    if sio.getvalue()[0] != '<' :
+        #        continue
+        #    analyzor = ET.ElementTree( file = sio )
+        #    sio.close()
 
-            item_list = analyzor.findall('garden/item')
+        #    item_list = analyzor.findall('garden/item')
 
-            for x in item_list:
-                if x.find('cropsid').text !='0' :
-                    if x.find('grow').text == x.find('totalgrow').text :
-                    #if True:
-                        if x.find('status').text == '1' and x.find('cropsstatus').text != '3' :
-                            item_index = self.list_ctrl.InsertStringItem( sys.maxint , "" )
-                            self.list_ctrl.SetStringItem( item_index , 0 ,  unicode( f_index[0] ) )
-                            self.list_ctrl.SetStringItem( item_index , 1 , f_index[1]  )
-                            self.list_ctrl.SetStringItem( item_index , 2 , x.find('farmnum').text  )
-                            try:
-                                self.list_ctrl.SetStringItem( 
-                                    item_index , 
-                                    3 , 
-                                    re.sub( r'<font.*>([^<]*)</?font>' , r'\1' ,  x.find('crops').text ) )
-                            except:
-                                continue
+        #    for x in item_list:
+        #        if x.find('cropsid').text !='0' :
+        #            if x.find('grow').text == x.find('totalgrow').text :
+        #            #if True:
+        #                if x.find('status').text == '1' and x.find('cropsstatus').text != '3' :
+        #                    item_index = self.list_ctrl.InsertStringItem( sys.maxint , "" )
+        #                    self.list_ctrl.SetStringItem( item_index , 0 ,  unicode( f_index[0] ) )
+        #                    self.list_ctrl.SetStringItem( item_index , 1 , f_index[1]  )
+        #                    self.list_ctrl.SetStringItem( item_index , 2 , x.find('farmnum').text  )
+        #                    try:
+        #                        self.list_ctrl.SetStringItem( 
+        #                            item_index , 
+        #                            3 , 
+        #                            re.sub( r'<font.*>([^<]*)</?font>' , r'\1' ,  x.find('crops').text ) )
+        #                    except:
+        #                        continue
 
 
-            #    for i , attrib in enumerate( kxData.global_config_info.getBasicAttribList() ):
-            #        self.list_ctrl.SetStringItem( item_index , i , x.find(attrib).text )
+        #    #    for i , attrib in enumerate( kxData.global_config_info.getBasicAttribList() ):
+        #    #        self.list_ctrl.SetStringItem( item_index , i , x.find(attrib).text )
 
-            #    if x.find( 'cropsid' ).text != '0':
-            #        for i1 , attrib in enumerate( kxData.global_config_info.getExtentAttribList() ):
-            #            self.list_ctrl.SetStringItem( item_index , i + i1 + 1 , x.find(attrib).text )
+        #    #    if x.find( 'cropsid' ).text != '0':
+        #    #        for i1 , attrib in enumerate( kxData.global_config_info.getExtentAttribList() ):
+        #    #            self.list_ctrl.SetStringItem( item_index , i + i1 + 1 , x.find(attrib).text )
 
 
 
     def on_close( self , event ):
         # Logout
-        #resp = kxData.SendRequest('Logout' )
+        kxData.global_network_config_info.friends_list.UpdateToFile()
+        kxData.global_network_config_info.logout()
         self.Destroy()
 
 
